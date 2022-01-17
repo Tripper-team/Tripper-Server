1806a56607a89b5664278e585940f4e4859d41b1
KangukLee-97 <rkddnrdl97@naver.com>
2022-01-17 23:24:31 +0900

    Pull local's code

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 // TODO: 본인의 DB 계정 입력
 let pool;
 
-if (process.env.NODE_ENV = 'production') {
+if (process.env.NODE_ENV === 'production') {
     pool = mysql.createPool({
         host: process.env.RDS_HOST,
         user: process.env.RDS_USER,
