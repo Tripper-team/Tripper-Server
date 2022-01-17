8c03d704334158057c0faec33cc4211decf6f483
KangukLee-97 <rkddnrdl97@naver.com>
2022-01-17 17:59:59 +0900

    Divide development/production using node environment value

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('./config/express');
 const {logger} = require('./config/winston');
 
-const port = 3000;
+let port;
+
+if (process.env.NODE_ENV === 'development')
+    port = 3001;
+else if (process.env.NODE_ENV === 'production')
+    port = 3002;
+else
+    port = 3000;
+
 express().listen(port);
 logger.info(`${process.env.NODE_ENV} - API Server Start At Port ${port}`);
