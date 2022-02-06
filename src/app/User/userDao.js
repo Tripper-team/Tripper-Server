30b805d37a3505e6ae0662df96692981765f1291
KangukLee-97 <rkddnrdl97@naver.com>
2022-02-06 11:31:28 +0900

    Update mypage query

--- src/app/User/userDao.js
+++ src/app/User/userDao.js
@@ -273,44 +273,34 @@ async function selectUserFeedInMyPageByOption(connection, [userIdx, option, star
   }
   else if (option === "좋아요") {
     selectUserFeedInMyPageByOptionQuery = `
-      SELECT travelIdx, likeStatus, travelTitle, travelIntroduce, travelHashtag, travelScore, thumImgUrl, temp.createdAt AS createdAt
+      SELECT travelIdx, userIdx, status, travelHashtag, score, thumImgUrl, likeCreatedAt
       FROM (
-             SELECT T.userIdx, travelIdx, IF(TravelLike.status = 'Y', '좋아요 활성화', '좋아요 비활성화') AS likeStatus, T.title AS travelTitle,
-                    T.introduce AS travelIntroduce, GROUP_CONCAT(CONCAT('#', H.content) SEPARATOR ' ') AS travelHashtag,
-                    CASE
-                      WHEN S.score IS NULL THEN null
-                      WHEN S.score < 2.0 THEN "별로에요"
-                      WHEN S.score < 3.0 THEN "도움되지 않았어요"
-                      WHEN S.score < 4.0 THEN "그저 그래요"
-                      WHEN S.score < 4.5 THEN "도움되었어요!"
-                      ELSE "최고의 여행!"
-                      END AS travelScore, thumImgUrl, TravelLike.createdAt AS createdAt
-             FROM TravelLike
-                    INNER JOIN Travel AS T ON TravelLike.travelIdx = T.idx AND T.status != 'DELETED' AND T.status != 'PRIVATE'
-        LEFT JOIN (
-            SELECT TravelHashtag.travelIdx AS hashTravelIdx, content
-            FROM TravelHashtag
-                INNER JOIN Hashtag ON Hashtag.idx = TravelHashtag.hashtagIdx
-            WHERE TravelHashtag.status = 'Y'
-        ) AS H ON T.idx = H.hashTravelIdx
-               LEFT JOIN (
-               SELECT TravelScore.travelIdx AS scoreTravelIdx, AVG(score) AS score
+             SELECT TL.travelIdx, TL.userIdx, TL.status, TL.createdAt AS likeCreatedAt,
+                    GROUP_CONCAT(CONCAT('#', TH.content) SEPARATOR ' ') AS travelHashtag, score, thumImgUrl
+             FROM TravelLike AS TL
+                    INNER JOIN Travel AS T ON TL.travelIdx = T.idx AND T.status = 'PUBLIC'
+                    LEFT JOIN (
+               SELECT TravelHashtag.travelIdx AS hashTravelIdx, content
+               FROM TravelHashtag
+                      INNER JOIN Hashtag ON Hashtag.idx = TravelHashtag.hashtagIdx
+               WHERE TravelHashtag.status = 'Y'
+             ) AS TH ON T.idx = TH.hashTravelIdx
+                    LEFT JOIN (
+               SELECT TravelScore.travelIdx, AVG(score) AS score
                FROM TravelScore
-               LEFT JOIN TravelLike ON TravelLike.travelIdx = TravelScore.travelIdx
+                      INNER JOIN User ON TravelScore.userIdx = User.idx AND User.isWithdraw = 'N'
                GROUP BY TravelScore.travelIdx
-               ) AS S ON H.hashTravelIdx = S.scoreTravelIdx
-               LEFT JOIN (
-               SELECT TravelThumnail.idx, TravelThumnail.travelIdx AS thumTravelIdx, thumImgUrl
+             ) AS TS ON TS.travelIdx = TH.hashTravelIdx
+                    LEFT JOIN (
+               SELECT TravelThumnail.idx, travelIdx, thumImgUrl
                FROM TravelThumnail
-               LEFT JOIN TravelLike ON TravelThumnail.travelIdx = TravelLike.travelIdx
-               GROUP BY TravelThumnail.travelIdx HAVING MIN(TravelThumnail.idx)
-               ) AS TH ON S.scoreTravelIdx = TH.thumTravelIdx
-               INNER JOIN User ON User.idx = T.userIdx AND User.isWithdraw = 'N'
-             WHERE TravelLike.userIdx = ? AND TravelLike.status = 'Y'
-             GROUP BY TravelLike.travelIdx
-           ) AS temp
-      ORDER BY temp.createdAt DESC 
-      LIMIT ?, ?;    
+               GROUP BY travelIdx HAVING MIN(TravelThumnail.idx)
+             ) AS TM ON TS.travelIdx = TM.travelIdx
+             WHERE TL.userIdx = ? AND TL.status = 'Y'
+             GROUP BY TL.travelIdx
+           ) AS A
+      ORDER BY likeCreatedAt
+      LIMIT ?, ?;  
       `;
     const [selectUserFeedInMyPageByOptionRow] = await connection.query(selectUserFeedInMyPageByOptionQuery, [userIdx, start, pageSize]);
     return selectUserFeedInMyPageByOptionRow;
