2e3db4ec7715bd47612d6bcde6201b4d15d5a4ca
KangukLee-97 <rkddnrdl97@naver.com>
2022-02-13 01:33:14 +0900

    Start mainPage API

--- src/app/Main/mainController.js
+++ src/app/Main/mainController.js
@@ -15,9 +15,9 @@ exports.getMainPage = async function (req, res) {
     const userIdx = req.verifiedToken.userIdx;
     let option = req.query.option;   // 최신순, 인기순, 팔로우
 
-    if (!option) option = '최신순';   // 메인페이지 첫 조회시 기본값은 최신순
-    if (option !== '최신순' && option !== '인기순' && option !== '팔로우')
-        return res.send(errResponse(baseResponse.));
+    // if (!option) option = '최신순';   // 메인페이지 첫 조회시 기본값은 최신순
+    // if (option !== '최신순' && option !== '인기순' && option !== '팔로우')
+    //     return res.send(errResponse(baseResponse.));
 
     // 사용자 회원탈퇴 유무 확인
 
